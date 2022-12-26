440f73080b9b8b336359412cf071b11961d25758
zegadb <zeygammehtiev@gmail.com>
2022-12-26 09:25:06 +0400

    clear readme file

---
+++ src/components/Nav.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useState } from 'react'
+
+const Nav = () => {
+  const [sidebar, setSidebar] = useState(false)
+  return (
+    <>
+      <header>
+        <div className="container flexSB">
+          <div className="logo">
+            <h1>
+              .<span>m</span>uu
+            </h1>
+          </div>
+          <nav>
+            <button className="toggle" onClick={() => setSidebar(!sidebar)}>
+              {sidebar ? <i className='fas fa-bars' /> : <i className='fas fa-times' />}
+            </button>
+            <ul className={sidebar ? 'nav-links-sidebar' : 'nav-links'} onClick={() => setSidebar(false)}>
+              <li><a href="/">Home</a></li>
+              <li><a href="/">About</a></li>
+              <li><a href="/">Creative</a></li>
+              <li><a href="/">Educations</a></li>
+              <li><a href="/">Works</a></li>
+              <li><a href="/">Blog</a></li>
+              <li><a href="/">Contacts</a></li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default Nav
